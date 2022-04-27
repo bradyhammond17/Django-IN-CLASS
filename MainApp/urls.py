a815d382c7fee5636c8279a9d2897ba33adf97fa
Brady Hammond <brady_hammond1@baylor.edu>
2022-04-27 15:35:32 -0500

    4/27 CLASS

--- MainApp/urls.py
+++ MainApp/urls.py
@@ -8,4 +8,6 @@ app_name = 'MainApp'
 urlpatterns = [
     path('', views.index, name = 'index'),
     path('topics', views.topics, name='topics'),
+    path('topics/<int:topic_id>/',views.topic, name='topic'),
+    path('new_topic/', views.new_topic, name='new_topic'),
 ]
--- MainApp/views.py
+++ MainApp/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from .forms import TopicForm
 from .models import Topic
 # Create your views here.
 
@@ -14,3 +16,25 @@ def topics(request):
     ##WHATEVER YOU USE AS A KEY IN THIS DICTIONARY IS USED IN HTML FILE, THE VALUE IS USED IN THE VIEW FILE
     
     return render(request, 'MainApp/topics.html', context)
+
+def topic(request, topic_id):
+    ## view must be consistent with url
+    topic = Topic.objects.get(id=topic_id)
+    entries = topic.entry_set.order_by('-date_added')
+
+    context = {'topic':topic,'entries':entries}
+
+    return render(request, 'MainApp/topic.html', context)
+
+def new_topic(request):
+    if request.method != 'POST':
+        form = TopicForm()
+    else:
+        form = TopicForm(data=request.POST)
+
+        if form.is_valid():
+            new_topic = form.save()
+
+            return redirect('MainApp:topics')
+    context = {'form':form}
+    return render(request, 'MainApp/new_topic.html', context)

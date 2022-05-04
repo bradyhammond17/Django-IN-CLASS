06d8a239a9c25345c75c7bfddce14f20366b4f91
Brady Hammond <brady_hammond1@baylor.edu>
2022-05-04 15:16:50 -0500

    5/4 class

--- MainApp/views.py
+++ MainApp/views.py
@@ -2,30 +2,38 @@ from django.shortcuts import render, redirect
 
 from .forms import EntryForm, TopicForm
 from .models import Topic, Entry
+from django.contrib.auth.decorators import login_required
+from django.http import Http404
+
 # Create your views here.
 
 def index(request):
     return render(request, 'MainApp/index.html')
 
-
+@login_required
 def topics(request):
-    topics = Topic.objects.all()
-
+    topics = Topic.objects.filter(owner=request.user).order_by('-date_added')
 
     context = {'topics': topics}
     ##WHATEVER YOU USE AS A KEY IN THIS DICTIONARY IS USED IN HTML FILE, THE VALUE IS USED IN THE VIEW FILE
     
     return render(request, 'MainApp/topics.html', context)
 
+@login_required
 def topic(request, topic_id):
     ## view must be consistent with url
     topic = Topic.objects.get(id=topic_id)
+
+    if topic.owner != request.user:
+        raise Http404
+
     entries = topic.entry_set.order_by('-date_added')
 
     context = {'topic':topic,'entries':entries}
 
     return render(request, 'MainApp/topic.html', context)
 
+@login_required
 def new_topic(request):
     if request.method != 'POST':
         form = TopicForm()
@@ -33,16 +41,21 @@ def new_topic(request):
         form = TopicForm(data=request.POST)
 
         if form.is_valid():
-            new_topic = form.save()
+            new_topic = form.save(commit=False)
+            new_topic.owner = request.user
+            new_topic.save()
 
             return redirect('MainApp:topics')
     context = {'form':form}
     return render(request, 'MainApp/new_topic.html', context)
 
-
+@login_required
 def new_entry(request, topic_id):
     topic = Topic.objects.get(id=topic_id)
 
+    if topic.owner != request.user:
+        raise Http404
+        
     if request.method != 'POST':
         form = EntryForm()
     else:
@@ -57,9 +70,13 @@ def new_entry(request, topic_id):
     context = {'form':form, 'topic':topic}
     return render(request, 'MainApp/new_entry.html', context)
 
+@login_required
 def edit_entry(request, entry_id):
     entry = Entry.objects.get(id=entry_id)
     topic = entry.topic
+
+    if topic.owner != request.user:
+        raise Http404
 
     if request.method != 'POST':
         form = EntryForm(instance=entry)
--- learning_log/settings.py
+++ learning_log/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     #my_apps
     'MainApp',
+    'users',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -120,3 +121,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+LOGIN_URL = 'users:login'
